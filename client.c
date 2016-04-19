9c4bbac39813817e56accffa2fa1717ee734bc80
h-betz <hhb311@gmail.com>
2016-04-19 10:51:12 -0400

    Updated command handling on client

--- client.c
+++ client.c
@@ -15,6 +15,144 @@
 #define PORT_FTP 8888
 #define SERVER_ADDR "127.0.0.1"
 
+char * readAccountName(char *str, int i) {
+    
+    char *acct_name = (char *)malloc(strlen(str) + 1);
+    bzero(acct_name, strlen(str) + 1);
+    int ind = 0;
+    char c = str[i];
+    if (isalnum(c)) {
+        
+        while (isalnum(c)) {
+            acct_name[ind] = c;
+            ind++;
+            c = str[++i];
+        }
+        
+        if (c == '\0') {
+            return acct_name;
+        }
+        
+    }
+    
+    return 0;
+}
+
+float readCreditDebit(char *str, int i) {
+    
+    char *amount = (char *) malloc(strlen(str) + 1);
+    bzero(amount, strlen(str) + 1);
+    int ind = 0;
+    char c = str[i];
+    
+    if (isdigit(c)) {
+        
+        while (isdigit(c)) {
+            amount[ind] = c;
+            ind++;
+            c = str[++i];
+        }
+        
+        if (c == '.') {
+            amount[ind] = c;
+            ind++;
+            c = str[++i];
+            while (isdigit(c)) {
+                amount[ind] = c;
+                ind++;
+                c = str[++i];
+            }
+            if (c == '\0') {
+                //return as float
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+        
+    } else if (c == '.') {
+        
+    }
+    
+    return -2;
+}
+
+int checkCommand(char *token, char *str, int i) {
+    
+    if (strcmp("open", token) == 0) {
+        //Command is open
+        char *acct_name = readAccountName(str, i);
+        return 0;
+    } else if (strcmp("start", token)) {
+        //Command is start
+        char *acct_name = readAccountName(str, i);        
+        return 1;
+    } else if (strcmp("debit", token)) {
+        //Command is debit
+        float debit = readCreditDebit(str, i);
+        return 2;
+    } else if (strcmp("credit", token)) {
+        //Command is credit
+        float credit = readCreditDebit(str, i);
+        return 3;
+    }
+
+    return -1;
+}
+
+//Parses user input to read commands
+void tokenize(char *str) {
+    
+    char *token = (char *) malloc(strlen(str));
+    bzero(token, strlen(str));
+    char c = str[0];
+    int i = 0;
+    int result = -2;
+    
+    //Move through each character of the string to read it
+    if (isalpha(c)) {
+        token[i] = c;
+        c = str[i];
+        while (isalpha(c)) {
+            token[++i] = 
+            c = str[i];
+        }
+        //If next character is a space, marks end of command (eg. debit, open, credit)
+        if (c == ' ') {
+            result = checkCommand(token, str, ++i);
+            free(token);
+            /*switch(result) {
+                case 0:
+                    //read account name
+                    break;
+                case 1:
+                    //start account
+                    break;
+                case 2:
+                    //readCreditDebit
+                    break;
+                case 3:
+                    //readCreditDebit
+                    break;
+                default:
+                
+            }*/
+            //switch case to handle command
+        } else if (c == '\0') {
+            if (strcmp("balance", token)) {
+                //Command is balance
+            } else if (strcmp("finish", token)) {
+                //Command is finish
+            }
+        } else {
+            //Print error -- Not a viable command
+        }
+        
+    }
+    
+}
+
 void * handleServer() {
     
     
